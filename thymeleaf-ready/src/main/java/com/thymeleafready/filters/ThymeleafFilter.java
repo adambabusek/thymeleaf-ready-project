3839e7a2c841a0eb233c8f93b1a6777183f267c0
adambabusek <adam.babusek@gmail.com>
2017-08-19 23:45:10 +0200

    first implementation of working ThymeleafFilter

--- thymeleaf-ready/src/main/java/com/thymeleafready/filters/ThymeleafFilter.java
+++ thymeleaf-ready/src/main/java/com/thymeleafready/filters/ThymeleafFilter.java
@@ -8,17 +8,42 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.IContext;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 public class ThymeleafFilter implements Filter {
 
+	private TemplateEngine templateEngine;
+
+	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-
-		chain.doFilter(request, response);
+		IContext ctx = new WebContext((HttpServletRequest) request,	(HttpServletResponse) response, request.getServletContext());
+		templateEngine.process("example", ctx, response.getWriter());
 	}
 
+	@Override
 	public void destroy() {	}
 
-	public void init(FilterConfig cfg) throws ServletException { }
+	/**
+	 * Initialization of ThymeleafFilter consists of template resolver and template engine creation.
+	 */
+	@Override
+	public void init(FilterConfig cfg) throws ServletException {
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(cfg.getServletContext());
+		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setCacheable(false); // cacheble=false during development
+
+		templateEngine = new TemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+	}
 
 }
